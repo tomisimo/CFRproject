d7ab5d47bafedf97732621f7cdc900507ff861e0
Simo Tamas <simo_tamas2007@yahoo.com>
2021-12-14 13:12:49 +0200

    Primele arrayuri !

--- src/tamas/Main.java
+++ src/tamas/Main.java
@@ -1,12 +1,25 @@
 package tamas;
 
-public class Main {
+import java.util.Scanner;
 
+public class Main {
     public static void main(String[] args) {
-        int a = 8;
-        int b = 9;
-        int c = 15;
-        System.out.println(a+b+c);
+        String editiileDeFotbal[] = {"2004-2005", "2005-2006", "2006-2007", "2007-2008", "2008-2009", "2009-2010"};
+        String echipeEditia2004_2005[] = {"FC STEAUA Bucuresti", "FC DINAMO Bucuresti", "FC RAPID Bucuresti",
+                "FC NATIONAL Bucuresti"};
+        for (int i = 0; i < editiileDeFotbal.length; i++) {
+            System.out.println(editiileDeFotbal[i]);
+        }
+        System.out.println("Alege editia !");
+        Scanner inputObject = new Scanner(System.in);
+        String theInput = inputObject.nextLine();
+
+        if(theInput.equals(editiileDeFotbal[0])){
+            for(int i =0; i < echipeEditia2004_2005.length; i++){
+                System.out.println(echipeEditia2004_2005[i]);
+            }
+        }
+
 
     }
 }
