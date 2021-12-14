211456998e72f41ccd487fc2177f3eefc00d30cb
Simo Tamas <simo_tamas2007@yahoo.com>
2021-12-14 11:28:48 +0200

    first code written

--- src/tamas/Main.java
+++ src/tamas/Main.java
@@ -3,6 +3,10 @@ package tamas;
 public class Main {
 
     public static void main(String[] args) {
-	
+        int a = 8;
+        int b = 9;
+        int c = 15;
+        System.out.println(a+b+c);
+
     }
 }
